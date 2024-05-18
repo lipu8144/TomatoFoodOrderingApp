2e10ce4eccb66949a11cdb7a0891a1623d895d99
Lipu Swain <lipunswain10@gmail.com>
2024-05-18 23:06:08 +0530

    backend link updated

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -9,7 +9,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = ({children}) =>{
 
     const [cartItems, setCartItems] = useState({})
-    const url = "http://localhost:4000";
+    const url = "https://tomato-backend-5krq.onrender.com";
     const [token, setToken] = useState("")
     const [food_list, setFoodList] = useState([])
 
