b81d1a72aa927ec05720333c29ffeb3d2388b040
Lipu Swain <lipunswain10@gmail.com>
2024-09-23 08:33:10 +0530

    loader added

--- frontend/src/components/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay.jsx
@@ -1,25 +1,37 @@
 import React, { useContext } from "react";
 import { StoreContext } from "../main";
 import FoodItem from "./FoodItem";
+import Loader from "./Loader";
 
 const FoodDisplay = ({category}) => {
     const {food_list} = useContext(StoreContext)
-  return <div className="food-display mt-8" id="food-display">
-    <h2 className="text-[max(2vw,24px)] font-semibold">Top dishes near you</h2>
-    <div className="food-display-list grid md:grid-cols-displayItems mt-8 gap-7 gap-y-12">
-        {
-            food_list.map((food, index) => {
-
-                if(category == "All" || category === food.category){
-
-                    return(
-                        <FoodItem key={index} id={food._id} name={food.name} description={food.description} price={food.price} image={food.image} />
-                    )
-                }
-            })
-        }
+  return (
+    <div className="food-display mt-8" id="food-display">
+      <h2 className="text-[max(2vw,24px)] font-semibold">
+        Top dishes near you
+      </h2>
+      <div className="food-display-list grid md:grid-cols-displayItems mt-8 gap-7 gap-y-12">
+        {!food_list ? (
+          <Loader />
+        ) : (
+          food_list.map((food, index) => {
+            if (category == "All" || category === food.category) {
+              return (
+                <FoodItem
+                  key={index}
+                  id={food._id}
+                  name={food.name}
+                  description={food.description}
+                  price={food.price}
+                  image={food.image}
+                />
+              );
+            }
+          })
+        )}
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default FoodDisplay;
